2fd05fa876d9eb56d184e31ddb94caf8491f38ce
IT-Engineer-K <76894075+IT-Engineer-K@users.noreply.github.com>
2021-10-26 22:34:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ function pressed() {
     now = new Date
     audio.play()
     setTimeout(pause, 100);
+    document.getElementById('time').innerHTML = now.getSeconds() + now.getMilliseconds() / 1000;
 }
 
 function pause() {
     audio.pause()
-    document.getElementById('time').innerHTML = now.getSeconds() + now.getMilliseconds() / 1000;
-    //alert(now.getSeconds() + now.getMilliseconds() / 1000)
 }
